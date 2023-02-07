507bccc60faf30cacc77edbcdbfcaef3851ba71c
Carloseduardogyn <123594790+Carloseduardogyn@users.noreply.github.com>
2023-02-07 10:07:55 -0300

    Update Uri1038.java
    Uri1038

--- Uri1038.java
+++ Uri1038.java
@@ -1,9 +1,38 @@
 package Uri;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Uri1038 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		int codigo = sc.nextInt();
+		int qtd = sc.nextInt();
+		double total;
+		
+		if (codigo ==1) {
+			total = qtd * 4;
+			System.out.printf("Total: R$ %.2f%n" , total);
+		}else if (codigo == 2) {
+			total = qtd * 4.50;
+			System.out.printf("Total: R$ %.2f%n" , total);
+		}else if (codigo == 3) {
+			total = qtd * 5;
+			System.out.printf("Total: R$ %.2f%n" , total);
+		}else if (codigo == 4) {
+			total = qtd * 2;
+			System.out.printf("Total: R$ %.2f%n" , total);
+		}else if (codigo == 5) {
+			total = qtd * 1.50;
+			System.out.printf("Total: R$ %.2f%n" , total);
+		}
+		
+	
+        sc.close();
 
 	}
 
